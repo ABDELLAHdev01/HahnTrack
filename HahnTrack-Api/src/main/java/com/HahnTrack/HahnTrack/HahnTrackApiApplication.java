3a25d615918661e0793908d37b670fd1ca168523
Salvador <abdellahelghoulamdev@gmail.com>
2025-06-19 18:08:30 +0100

    [ HT-API ] : initialize backend project with Spring Boot and essential dependencies

---
+++ HahnTrack-Api/src/main/java/com/HahnTrack/HahnTrack/HahnTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.HahnTrack.HahnTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahnTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HahnTrackApiApplication.class, args);
+	}
+
+}
---
+++ HahnTrack-Api/src/test/java/com/HahnTrack/HahnTrack/HahnTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HahnTrack.HahnTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahnTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
